ae04a7ef7808ee6ec677829e9b6842c0ad46a15f
flyptkarsh <ptkarsh@gmail.com>
2016-05-10 13:09:18 -0400

    added bongia translation

--- app/assets/javascripts/story.js
+++ app/assets/javascripts/story.js
@@ -1,17 +1,28 @@
 angular.module('storyApp', []).controller('ngStoryController', function($scope, $http) {
 
-  $http({
-    method: 'GET',
-    url: '/story'
-  }).then(function successCallback(response) {
-      $scope.spanABTop = response.data.page.content[1].collections[0].assets
-      $scope.aColumn = response.data.page.content[1].collections[1].assets
-      $scope.bColumn = response.data.page.content[1].collections[2].assets
-      $scope.cColumn = response.data.page.content[1].collections[3].assets
-      $scope.liveMobileResults = response.data.page.content.length
-    }, function errorCallback(response) {
-      console.log(response);
-      $scope.story = response;
+  $scope.get_articles = function (language, more) {
+    console.log(language, more);
+    $http({
+      method: 'GET',
+      url: '/story',
+      params: { lang: language, more: more}
+    }).then(function successCallback(response) {
+        $scope.spanABTop = response.data[0];
+        $scope.aColumn = response.data[1];
+        $scope.bColumn = response.data[2];
+        $scope.cColumn =  response.data[3];
+        $scope.lang = language;
+      }, function errorCallback(response) {
+        $scope.story = response;
 
-    });
+      });
+  }
+
+  $scope.get_more_articles = function (language, more) {
+    $scope.get_articles(language, more)
+  }
+
+  $scope.get_articles("English", false);
+
+
 });
--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -1,14 +1,60 @@
 class StoryController < ApplicationController
+  require 'open-uri'
+  require 'json'
 
   def index
-    @stories = HTTParty.get('http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion2.js')
-    @old_stories = HTTParty.get('http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js')
 
-    render :json => @stories.parsed_response
+    stories = HTTParty.get('http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion2.js')
+    collections = JSON.parse( stories )["page"]["content"][1]["collections"]
+
+    if params[:lang] == "Bongia"
+      spanABTop = translate_assets_to_bongia( collections[0]["assets"])
+      aColumn = translate_assets_to_bongia( collections[1]["assets"])
+      bColumn = translate_assets_to_bongia( collections[2]["assets"])
+      cColumn = translate_assets_to_bongia( collections[3]["assets"])
+    else
+      spanABTop = collections[0]["assets"]
+      aColumn =  collections[1]["assets"]
+      bColumn =  collections[2]["assets"]
+      cColumn =  collections[3]["assets"]
+    end
+    @stories = [ spanABTop, aColumn, bColumn, cColumn ]
+    render :json => @stories
   end
 
   def show
    render  "story/show"
   end
+
+  private
+
+  def translate_assets_to_bongia(assets)
+    assets = assets.map do |a|
+      a["headline"] = convert_to_bongia(a["headline"])
+      a["original_headline"] = convert_to_bongia(a["original_headline"])
+      a["summary"] = convert_to_bongia(a["summary"])
+      a["original_summary"] = convert_to_bongia(a["original_summary"])
+      a
+    end
+    return assets
+  end
+
+  def convert_to_bongia(string)
+    a = string.split(" ")
+    new_string = ""
+    a.collect do |w|
+      if w.length > 3
+        if w.slice(0) == w.slice(0).upcase
+          new_string = new_string + " Bongia"
+        else
+          new_string = new_string + " bongia"
+        end
+      else
+        new_string = new_string + " " + w
+      end
+    end
+    return new_string
+  end
+
 
 end
